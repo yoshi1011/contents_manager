f55d65d7c1f72a6448f59cfb017523429af045e1
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2024-02-12 00:20:05 +0900

    Vue, Viteのアップデート

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import RubyPlugin from 'vite-plugin-ruby'
 
 export default defineConfig({
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+  },
   plugins: [
     vue(),
     RubyPlugin(),
